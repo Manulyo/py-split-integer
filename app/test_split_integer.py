f66effc50560df1a959e040a3f34a6ee30a6ece8
Oleksandr <15bizon15@gmail.com>
2025-11-14 17:09:06 +0100

    Fix multiple spaces before operator on 11 line

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,7 +8,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     result = split_integer(15, 5)
-    assert result  == [3, 3, 3, 3, 3]
+    assert result == [3, 3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
